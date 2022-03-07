9679bad2137f704a93692df09aaef3d219d1c7b8
이현욱 <lhu0505@naver.com>
2022-03-07 10:34:47 +0900

    exam01 02 03

--- test01/src/exam01/Sample01.java
+++ test01/src/exam01/Sample01.java
@@ -1,11 +1,8 @@
-package pro01;
+package exam01;
 
 public class Sample01 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
-				
-		
 	}
-
 }
---
+++ test01/src/exam03/Sample01.java
@@ -0,0 +1,48 @@
+package exam03;
+
+public class Sample01 {
+
+	public static void main(String[] args) {
+
+		int n1;
+		
+		int n2 = 1;
+		
+		int n3;
+		n3 = 2;
+		
+		n1 = 3;
+		/*
+		 * 자료형
+		 * boolean
+		 * char
+		 * String
+		 * byte 1byte
+		 * short 2 
+		 * int 4 
+		 * long 8
+		 * float
+		 * double
+		 * 그 외 객채들
+		 */
+		boolean b1 = true;
+		boolean b2 = false;
+		
+		
+		char c1 = 'a';
+		String s1 = "a love";
+		
+		byte b3 = 1;
+		short s2 = 2;
+		int i1 = 3;
+		long l1 = 4;
+		long l2 = 4L; // 접두사로 L을 생략해도 된다.
+		
+		float f1 = 5.0f; // 접두사로 f를 반드시 작성해야 한다.
+		double d1 = 6.0;
+		
+		
+	}
+	
+
+}
