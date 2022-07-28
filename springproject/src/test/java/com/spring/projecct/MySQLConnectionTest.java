2f8b53753066523f9069a45c7c835868e8d451db
lhu0505 <lhu0505@DESKTOP-5QF6C1F>
2022-07-28 22:40:45 +0900

    spring sql connection

---
+++ springproject/src/test/java/com/spring/projecct/MySQLConnectionTest.java
@@ -0,0 +1,30 @@
+package com.spring.projecct;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.jupiter.api.Test;
+
+public class MySQLConnectionTest {
+
+    // MySQL Connector 의 클래스. DB 연결 드라이버 정의
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    // DB 경로
+    private static final String URL = "jdbc:mysql://localhost:3306/user?serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false";
+    private static final String USER = "root";
+    private static final String PASSWORD = "7011";
+
+    @Test
+    public void testConnection() throws Exception {
+        // DBMS에게 DB 연결 드라이버의 위치를 알려주기 위한 메소드
+        Class.forName(DRIVER);
+        try {
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println(connection);
+            System.out.println(USER);
+            System.out.println("연결 성공");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
